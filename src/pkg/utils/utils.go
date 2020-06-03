32ad0fb0509f5202d54d3fc74baa16e800ae9469
Jeroen van Maanen <jeroen@leialearns.org>
2020-06-03 07:52:35 +0200

    Added function utils#ProtoCast.

--- src/pkg/utils/utils.go
+++ src/pkg/utils/utils.go
@@ -1,10 +1,22 @@
 package utils
 
-import "log"
+import (
+	"log"
+
+	"github.com/golang/protobuf/proto"
+)
 
 func ReportError(label string, f func() error) {
 	e := f()
 	if e != nil {
 		log.Printf("%v: Error: %v", label, e)
 	}
+}
+
+func ProtoCast(source proto.Message, destination proto.Message) error {
+	bytes, e := proto.Marshal(source)
+	if e != nil {
+		return e
+	}
+	return proto.Unmarshal(bytes, destination)
 }
